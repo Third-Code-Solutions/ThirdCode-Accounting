bab5ad519a48177e00ca84024262c5b40dd092e2
Kurt <kurt@thirdcodesolutions.com>
2026-09-22 21:38:03 +0800

    fix: make TCSI landing page the public root

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,7 @@
-import { redirect } from "next/navigation";
-
 import { LandingPage } from "../components/landing-page";
-import { isPilotPortal } from "../lib/pilot";
 
 export const dynamic = "force-dynamic";
 
 export default function HomePage() {
-  if (isPilotPortal()) {
-    return <LandingPage />;
-  }
-  redirect("/dashboard");
+  return <LandingPage />;
 }
