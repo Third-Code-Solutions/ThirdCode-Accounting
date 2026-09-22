bc3ce17f06ffcafceccbabaf08a139c911c73f8a
Kurt <kurtgavin.design@gmail.com>
2026-09-22 18:03:20 +0800

    fix: persist branded system sender on existing databases

--- addons/thirdcode_accounting/__manifest__.py
+++ addons/thirdcode_accounting/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "TCSI Accounting",
     "summary": "Third Code Solutions Inc. accounting workspace",
-    "version": "18.0.2.5.3",
+    "version": "18.0.2.5.4",
     "category": "Accounting/Accounting",
     "author": "Third Code Solutions Inc.",
     "license": "AGPL-3",
--- addons/thirdcode_accounting/models/branding.py
+++ addons/thirdcode_accounting/models/branding.py
@@ -1,6 +1,8 @@
+import base64
 import re
 
 from odoo import api, models
+from odoo.tools import file_open
 
 
 def brand_invitation(body):
@@ -22,6 +24,11 @@ class CompanyBranding(models.Model):
     @api.model
     def _apply_tcsi_invitation_branding(self):
         # Private migration entry point: not callable through external RPC.
+        with file_open("thirdcode_accounting/static/src/img/orvexa-avatar.png", "rb") as avatar:
+            self.env.ref("base.partner_root").write({
+                "name": "TCSI Workspace Assistant",
+                "image_1920": base64.b64encode(avatar.read()),
+            })
         for xmlid in (
             "auth_signup.set_password_email",
             "auth_signup.mail_template_user_signup_account_created",
